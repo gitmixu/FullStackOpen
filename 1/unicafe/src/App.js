7f4ac2db36f65df47182c3fd4864aae125a120b3
mixu <97851078+mx-u@users.noreply.github.com>
2023-06-14 22:31:21 +0300

    anekdootit step1

--- 1/unicafe/src/App.js
+++ 1/unicafe/src/App.js
@@ -1,50 +1,24 @@
-import { useState } from 'react';
-import './app.css';
-
-const StatisticLine = (props) => {
-  console.log('StatisticLine', props);
-  return(
-    <tr>
-      <td id='text'>{props.text}</td>
-      <td>{props.value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({good, neutral, bad}) => {
-  console.log('stats:', good, neutral, bad);
-  let all = good + neutral + bad;
-  if (all < 1) {return (<div><p>No feedback given</p></div>)}
-  return(
-    <div>
-      <table>
-        <tbody>
-          <StatisticLine text="good" value ={good} />
-          <StatisticLine text="neutral" value ={neutral} />
-          <StatisticLine text="bad" value ={bad} />
-          <StatisticLine text="all" value ={all} />
-          <StatisticLine text="average" value ={((good + 0 - bad) / all).toFixed(1)} />
-          <StatisticLine text="positive" value ={((good / all * 100).toFixed(1)) + " %"}/>
-        </tbody>
-      </table>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}> good</button>
-      <button onClick={() => setNeutral(neutral + 1)}> neutral</button>
-      <button onClick={() => setBad(bad + 1)}> bad</button>
-      {console.log('App:','good',good,'neutral',neutral,'bad',bad)}
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {anecdotes[selected]}
+      <br></br>
+      <button onClick={() => setSelected(Math.floor(Math.random() * ((anecdotes.length - 1) - 0 + 1)) + 0)}>next anecdote</button>
     </div>
   )
 }
